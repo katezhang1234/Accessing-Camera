5e82aa8bba9512634142d8201dc17a2888a439a5
Kate Zhang <iamkate1226@gmail.com>
2021-06-28 01:00:00 -0700

    Initial Commit

---
+++ Accessing Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Accessing Camera
+//
+//  Created by Kate Zhang on 6/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
